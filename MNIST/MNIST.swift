65a875b42fe84fa405c00e555ef052936b1aeb9b
Daniel Zheng <danielzheng@google.com>
2018-04-25 03:45:33 -0700

    Fix data loading for MNIST model.
    - Use official training data from http://yann.lecun.com/exdb/mnist.
    - Use Foundation APIs instead of Numpy. Data conversion from Numpy -> Python -> Swift was prohibitively slow, now data reading is near instantaneous.

--- MNIST/MNIST.swift
+++ MNIST/MNIST.swift
@@ -1,37 +1,33 @@
 #!/usr/bin/env swift -O
-/*
- Copyright 2018 The TensorFlow Authors. All Rights Reserved.
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
+// Copyright 2018 The TensorFlow Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
  
-import Python
+import Foundation
 import TensorFlow
 
-public func readMnist(_ filename: String) -> (Tensor<Float>, Tensor<Int32>) {
-  let gzip = Python.import("gzip")
-  let pickle = Python.import("cPickle")
-
+public func readMnist(
+  imagesFile: String, labelsFile: String
+) -> (Tensor<Float>, Tensor<Int32>) {
   print("Reading data.")
-  let file = gzip.open.call(with: filename, "rb")
-  let serialized = pickle.load.call(with: file).tuple4
-  let (pyImages, pyLabels, pyRows, pyColumns) = serialized
-
-  print("Converting data to Swift.")
-  let images: [Float] = Array(pyImages)!
-  let labels: [Int32] = Array(pyLabels)!
-  let rowCount = Int32(pyRows)!
-  let columnCount = Int32(pyColumns)!
+  let imageData =
+    try! Data(contentsOf: URL(fileURLWithPath: imagesFile)).dropFirst(16)
+  let labelData =
+    try! Data(contentsOf: URL(fileURLWithPath: labelsFile)).dropFirst(8)
+  let images = imageData.map { Float($0) }
+  let labels = labelData.map { Int32($0) }
+  let rowCount = Int32(labels.count)
+  let columnCount = Int32(images.count) / rowCount
 
   print("Constructing data tensors.")
   let imagesTensor = Tensor(shape: [rowCount, columnCount], scalars: images)
@@ -41,19 +37,22 @@ public func readMnist(_ filename: String) -> (Tensor<Float>, Tensor<Int32>) {
 
 func main() {
   // Training data
-  // - Note: There are two options for MNIST data for open-source.
+  let currentDirectory =
+    URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
+  let currentScriptPath =
+    URL(fileURLWithPath: CommandLine.arguments[0], relativeTo: currentDirectory)
+  let scriptDirectory = currentScriptPath.appendingPathComponent("..")
 
-  // - Option 1: Use unmodified MNIST data from:
-  //   http://deeplearning.net/tutorial/gettingstarted.html.
-  // let (images, numericLabels) = readMnistOriginal("mnist.pkl.gz")
-  // let labels = Tensor<Float>(oneHotAtIndices: numericLabels, depth: 10)
-
-  // - Option 2: Use preprocessed MNIST data.
-  let (images, numericLabels) = readMnist("mnist.1000.pkl.gz")
+  let imagesFile =
+    scriptDirectory.appendingPathComponent("train-images-idx3-ubyte").path
+  let labelsFile =
+    scriptDirectory.appendingPathComponent("train-labels-idx1-ubyte").path
+  let (images, numericLabels) = readMnist(imagesFile: imagesFile,
+                                          labelsFile: labelsFile)
   let labels = Tensor<Float>(oneHotAtIndices: numericLabels, depth: 10)
 
   // Hyper-parameters
-  let iterationCount: Int32 = 10
+  let iterationCount: Int32 = 20
   let learningRate: Float = 0.2
   var loss = Float.infinity
 
@@ -63,7 +62,9 @@ func main() {
   var b1 = Tensor<Float>(zeros: [1, 30])
   var b2 = Tensor<Float>(zeros: [1, 10])
 
-  // Training loop
+  // Training loop.
+  print("Begin training for \(iterationCount) iterations.")
+
   var i: Int32 = 0
   repeat {
     // Forward pass
@@ -86,14 +87,14 @@ func main() {
     w2 -= dw2 * learningRate
     b2 -= db2 * learningRate
 
-    // Update current loss
+    // Update loss
     loss = dz2.squared().mean(squeezingAxes: 1, 0).scalarized()
 
     // Update iteration count
     i += 1
   } while i < iterationCount
 
-  // Check results
+  // Print loss
   print("Loss: \(loss)")
 }
 
