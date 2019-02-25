b6f7150e7013b15391f6ca361aa30556e5a8524b
Mingsheng Hong <hongm@google.com>
2019-02-25 11:59:35 -0800

    Addressed feedback.
    Also switched from SGD optimizer to Adam for more training efficiency. Now we
    only need ~25 mini batches instead of ~150.

--- Gym/CartPole.swift
+++ Gym/CartPole.swift
@@ -18,47 +18,52 @@ import TensorFlow
 let np = Python.import("numpy")
 let gym = Python.import("gym")
 
-let HIDDEN_SIZE = 128
-let BATCH_SIZE = 16
-let PERCENTILE = 70
+/// Model parameters and hyper parameters.
+let hiddenSize = 128
+let batchSize = 16
+/// Controls the amount of good/long episodes to retain for training.
+let percentile = 70
 
 func scalarFromPython<Scalar: PythonConvertible>(value: PythonObject) -> Scalar {
   let swiftValue = Scalar.init(value)
+  // Before force-unwrapping, we use `assert()` to make unwrapping nil more
+  // debuggable (assert points to the source location.)
   assert(swiftValue != nil)
   return swiftValue!
 }
 
 func tensorFromPython<Scalar: NumpyScalarCompatible>(numpy: PythonObject) -> Tensor<Scalar> {
   let tensor = Tensor<Scalar>(numpy: numpy)
+  // Before force-unwrapping, we use `assert()` to make unwrapping nil more
+  // debuggable (assert points to the source location.)
   assert(tensor != nil)
   return tensor!
 }
 
-// A simple two layer dense net.
-public struct Net: Layer {
+/// A simple two layer dense net.
+struct Net: Layer {
   var l1, l2: Dense<Float>
 
-  public init(obsSize: Int, hiddenSize: Int, actionCount: Int) {
+  init(observationSize: Int, hiddenSize: Int, actionCount: Int) {
     self.l1 = Dense<Float>(
-      inputSize: obsSize, outputSize: hiddenSize, activation: { $0 })
+      inputSize: observationSize, outputSize: hiddenSize, activation: relu)
 
     self.l2 = Dense<Float>(
       inputSize: hiddenSize, outputSize: actionCount, activation: { $0 })
   }
 
   @differentiable(wrt: (self, input))
-  public func applied(to input: Tensor<Float>, in context: Context) -> Tensor<Float> {
-    return l2.applied(to: relu(l1.applied(to: input, in: context)), in: context)
+  func applied(to input: Tensor<Float>, in context: Context) -> Tensor<Float> {
+    return l2.applied(to: l1.applied(to: input, in: context), in: context)
   }
 }
 
-// An episode is a list of steps, where each step records the observation from
-// env and the action taken. They will serve respectively as the input and
-// target (label) of the neural net training.
-// 
+/// An episode is a list of steps, where each step records the observation from
+/// env and the action taken. They will serve respectively as the input and
+/// target (label) of the neural net training.
 struct Episode {
   struct Step {
-    let obs: Tensor<Float>
+    let observation: Tensor<Float>
     let action: Int32
   }
 
@@ -66,10 +71,16 @@ struct Episode {
   let reward: Float
 }
 
-func filterBatch(episodes: [Episode], actionCount: Int32
-) -> (input: Tensor<Float>, target: Tensor<Float>, episodeCount: Int, meanReward: Float) {
+/// Filter out bad/short episodes before we feed them as neural net training
+/// data.
+func filteringBatch(episodes: [Episode],
+                    actionCount: Int32
+) -> (input: Tensor<Float>,
+      target: Tensor<Float>,
+      episodeCount: Int,
+      meanReward: Float) {
   let rewards = episodes.map { $0.reward }
-  let rewardBound = Float(np.percentile(rewards, PERCENTILE))!
+  let rewardBound = Float(np.percentile(rewards, percentile))!
   print("rewardBound = \(rewardBound)")
 
   var input = Tensor<Float>(0.0)
@@ -82,19 +93,19 @@ func filterBatch(episodes: [Episode], actionCount: Int32
       continue
     }
 
-    let obsTensor = Tensor<Float>(episode.steps.map { $0.obs })
-    let actsTensor = Tensor<Int32>(episode.steps.map { $0.action })
-    let oneHotLabels = Tensor<Float>(oneHotAtIndices: actsTensor, depth: actionCount)
+    let observationTensor = Tensor<Float>(episode.steps.map { $0.observation })
+    let actionTensor = Tensor<Int32>(episode.steps.map { $0.action })
+    let oneHotLabels = Tensor<Float>(oneHotAtIndices: actionTensor, depth: actionCount)
 
-    // print("observations tensor has shape \(obsTensor.shapeTensor)")
-    // print("actions tensor has shape \(actsTensor.shapeTensor)")
+    // print("observations tensor has shape \(observationTensor.shapeTensor)")
+    // print("actions tensor has shape \(actionTensor.shapeTensor)")
     // print("onehot actions tensor has shape \(oneHotLabels.shapeTensor)")
 
     if retainedEpisodeCount == 0 {
-      input = obsTensor
+      input = observationTensor
       target = oneHotLabels
     } else {
-      input = input.concatenated(with: obsTensor)
+      input = input.concatenated(with: observationTensor)
       target = target.concatenated(with: oneHotLabels)
     }
     // print("input tensor has shape \(input.shapeTensor)")
@@ -107,16 +118,17 @@ func filterBatch(episodes: [Episode], actionCount: Int32
   return (input, target, retainedEpisodeCount, totalReward / Float(retainedEpisodeCount))
 }
 
-// We pass in `net` by reference to avoid making a copy, even though this function is
-// running inferencing over net, and not mutating it.
-func nextBatch(env: PythonObject, net: inout Net, batchSize: Int, actionCount: Int32
+func nextBatch(env: PythonObject,
+               net: Net,
+               batchSize: Int,
+               actionCount: Int32
 ) -> [Episode] {
   var obsNumpy = env.reset()
 
   let context = Context(learningPhase: .inference)
   var episodes: [Episode] = []
   
-  // We build up a batch of observations and actions
+  // We build up a batch of observations and actions.
   for _ in 0..<batchSize {
     var steps: [Episode.Step] = []
     var episodeReward: Float = 0.0
@@ -139,7 +151,7 @@ func nextBatch(env: PythonObject, net: inout Net, batchSize: Int, actionCount: I
       let observationFloat = Tensor<Float>(observationDouble)
       let action: Int64 = scalarFromPython(value: actionNumpy)
 
-      steps.append(Episode.Step(obs: observationFloat, action: Int32(action)))
+      steps.append(Episode.Step(observation: observationFloat, action: Int32(action)))
 
       episodeReward += scalarFromPython(value: reward)
 
@@ -157,45 +169,38 @@ func nextBatch(env: PythonObject, net: inout Net, batchSize: Int, actionCount: I
   return episodes
 }
 
-func loss(model: Net, input: Tensor<Float>, target: Tensor<Float>)
-  -> Tensor<Float> {
-  let context = Context(learningPhase: .training)
-  let logits = model.applied(to: input, in: context)
-  return softmaxCrossEntropy(logits: logits, labels: target)
-}
+let env = gym.make("CartPole-v0")
+let observationSize: Int32 = scalarFromPython(value: env.observation_space.shape[0])
 
-func train() {
-  let env = gym.make("CartPole-v0")
-  let obsSize: Int32 = scalarFromPython(value: env.observation_space.shape[0])
+let actionCount: Int32 = scalarFromPython(value: env.action_space.n)
+// print(actionCount)
 
-  let actionCount: Int32 = scalarFromPython(value: env.action_space.n)
-  // print(actionCount)
+var net = Net(observationSize: Int(observationSize), hiddenSize: hiddenSize, actionCount: Int(actionCount))
+/// SGD optimizer reaches convergence with ~125 mini batches, while Adam uses ~25.
+// let optimizer = SGD<Net, Float>(learningRate: 0.1, momentum: 0.9)
+let optimizer = Adam<Net, Float>(learningRate: 0.01)
+var batchIndex = 0
+while true {
+  print("Processing mini batch \(batchIndex)")
+  batchIndex += 1
+  
+  let episodes = nextBatch(env: env, net: net, batchSize: batchSize, actionCount: actionCount)
+  let (input, target, episodeCount, meanReward) = filteringBatch(
+    episodes: episodes, actionCount: actionCount)
 
-  var net = Net(obsSize: Int(obsSize), hiddenSize: HIDDEN_SIZE, actionCount: Int(actionCount))
-  let optimizer = SGD<Net, Float>(learningRate: 0.1, momentum: 0.9)
-  var batchIndex = 0
-  while true {
-    print("Processing mini batch \(batchIndex)")
-    batchIndex += 1
-    
-    let episodes = nextBatch(env: env, net: &net, batchSize: BATCH_SIZE, actionCount: actionCount)
-    let (input, target, episodeCount, meanReward) = filterBatch(
-      episodes: episodes, actionCount: actionCount)
+  let gradients = gradient(at: net) { model -> Tensor<Float> in
+    let context = Context(learningPhase: .training)
+    let logits = model.applied(to: input, in: context)
+    let loss = softmaxCrossEntropy(logits: logits, labels: target)
+    print("loss is \(loss)")
+    return loss
+  }
+  optimizer.update(&net.allDifferentiableVariables, along: gradients)
 
-    let gradients = gradient(at: net) { model -> Tensor<Float> in
-      let l = loss(model: model, input: input, target: target)
-      print("loss is \(l)")
-      return l
-    }
-    optimizer.update(&net.allDifferentiableVariables, along: gradients)
+  print("It has episode count \(episodeCount) and mean reward \(meanReward)")
 
-    print("It has episode count \(episodeCount) and mean reward \(meanReward)")
-
-    if meanReward > 199 {
-      print("Solved")
-      break
-    }
+  if meanReward > 199 {
+    print("Solved")
+    break
   }
 }
-
-train()
