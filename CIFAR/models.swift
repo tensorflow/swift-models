c550f3cf86a44d41d41f0db1fb769fa201f79726
James Bradbury <jekbradbury@google.com>
2019-03-05 16:22:11 -0800

    temporarily switch away from sequenced

--- CIFAR/models.swift
+++ CIFAR/models.swift
@@ -1,7 +1,7 @@
 import TensorFlow
 
 // Ported from pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
-public struct PyTorchModel : Layer {
+public struct PyTorchModel: Layer {
     var conv1 = Conv2D<Float>(filterShape: (5, 5, 3, 6), activation: relu)
     var pool = MaxPool2D<Float>(poolSize: (2, 2), strides: (2, 2))
     var conv2 = Conv2D<Float>(filterShape: (5, 5, 6, 16), activation: relu)
@@ -12,19 +12,30 @@ public struct PyTorchModel : Layer {
 
     @differentiable
     public func applied(to input: Tensor<Float>, in context: Context) -> Tensor<Float> {
-        let convolved = input.sequenced(in: context, through: conv1, pool, conv2, pool)
-        return convolved.sequenced(in: context, through: flatten, dense1, dense2, dense3)
+        var tmp = input
+        tmp = conv1.applied(to: tmp, in: context)
+        tmp = pool.applied(to: tmp, in: context)
+        tmp = conv2.applied(to: tmp, in: context)
+        tmp = pool.applied(to: tmp, in: context)
+        tmp = flatten.applied(to: tmp, in: context)
+        tmp = dense1.applied(to: tmp, in: context)
+        tmp = dense2.applied(to: tmp, in: context)
+        tmp = dense3.applied(to: tmp, in: context)
+        return tmp
+        // blocked by TF-340
+        // let convolved = input.sequenced(in: context, through: conv1, pool, conv2, pool)
+        // return convolved.sequenced(in: context, through: flatten, dense1, dense2, dense3)
     }
 }
 
 // Ported from github.com/keras-team/keras/blob/master/examples/cifar10_cnn.py
-public struct KerasModel : Layer {
+public struct KerasModel: Layer {
     var conv1a = Conv2D<Float>(filterShape: (3, 3, 3, 32), padding: .same, activation: relu)
     var conv1b = Conv2D<Float>(filterShape: (3, 3, 32, 32), activation: relu)
     var pool1 = MaxPool2D<Float>(poolSize: (2, 2), strides: (2, 2))
     var dropout1 = Dropout<Float>(probability: 0.25)
     var conv2a = Conv2D<Float>(filterShape: (3, 3, 32, 64), padding: .same, activation: relu)
-    var conv2b = Conv2D<Float>(filterShape: (3, 3, 64, 64), activation: relu))
+    var conv2b = Conv2D<Float>(filterShape: (3, 3, 64, 64), activation: relu)
     var pool2 = MaxPool2D<Float>(poolSize: (2, 2), strides: (2, 2))
     var dropout2 = Dropout<Float>(probability: 0.25)
     var flatten = Flatten<Float>()
@@ -34,8 +45,23 @@ public struct KerasModel : Layer {
 
     @differentiable(wrt: (self, input))
     public func applied(to input: Tensor<Float>, in context: Context) -> Tensor<Float> {
-        let conv1 = input.sequenced(in: context, through: conv1a, conv1b, pool1, dropout1)
-        let conv2 = conv1.sequenced(in: context, through: conv2a, conv2b, pool2, dropout2)
-        return conv2.sequenced(in: context, through: flatten, dense1, dropout3, dense2)
+        var tmp = input
+        tmp = conv1a.applied(to: tmp, in: context)
+        tmp = conv1b.applied(to: tmp, in: context)
+        tmp = pool1.applied(to: tmp, in: context)
+        tmp = dropout1.applied(to: tmp, in: context)
+        tmp = conv2a.applied(to: tmp, in: context)
+        tmp = conv2b.applied(to: tmp, in: context)
+        tmp = pool2.applied(to: tmp, in: context)
+        tmp = dropout2.applied(to: tmp, in: context)
+        tmp = flatten.applied(to: tmp, in: context)
+        tmp = dense1.applied(to: tmp, in: context)
+        tmp = dropout3.applied(to: tmp, in: context)
+        tmp = dense2.applied(to: tmp, in: context)
+        return tmp
+        // blocked by TF-340
+        // let conv1 = input.sequenced(in: context, through: conv1a, conv1b, pool1, dropout1)
+        // let conv2 = conv1.sequenced(in: context, through: conv2a, conv2b, pool2, dropout2)
+        // return conv2.sequenced(in: context, through: flatten, dense1, dropout3, dense2)
     }
 }
