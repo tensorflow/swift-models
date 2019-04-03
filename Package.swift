aa59fa88426fa2ceadb76a32f5cd7ec3938665c8
Jianwei Xie <xiejw0217@gmail.com>
2019-04-03 13:13:29 -0700

    Add product for package.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "TensorFlowModels",
+    products: [
+        .executable(name: "MNIST", targets: ["MNIST"]),
+    ],
     targets: [
         .target(
             name: "MNIST",
