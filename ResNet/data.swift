26a16ce8943d15c1db8d57429bc5ba8e26b588e5
Richard Wei <rxwei@google.com>
2019-04-28 18:03:43 -0700

    Add copyright headers and tidy up ResNet.

--- ResNet/data.swift
+++ ResNet/data.swift
@@ -1,3 +1,17 @@
+// Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import Python
 import TensorFlow
 
@@ -10,7 +24,10 @@ func downloadCIFAR10IfNotPresent(to directory: String = ".") {
     let isdir = Bool(path.isdir(filepath))!
     if !isdir {
         print("Downloading CIFAR data...")
-        let command = "wget -nv -O- https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz | tar xzf - -C \(directory)"
+        let command = """
+            wget -nv -O- https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz | tar xzf - \
+            -C \(directory)
+            """
         subprocess.call(command, shell: true)
     }
 }
@@ -60,8 +77,7 @@ func loadCIFARTestFile() -> Example {
     return loadCIFARFile(named: "test_batch")
 }
 
-func loadCIFAR10() -> (
-    training: Dataset<Example>, test: Dataset<Example>) {
+func loadCIFAR10() -> (training: Dataset<Example>, test: Dataset<Example>) {
     let trainingDataset = Dataset<Example>(elements: loadCIFARTrainingFiles())
     let testDataset = Dataset<Example>(elements: loadCIFARTestFile())
     return (training: trainingDataset, test: testDataset)
