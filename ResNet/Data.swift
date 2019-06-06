09b9692355d21bde90bb6f04b37976320bd5ba8b
Leo Zhao <xueliang.zhao@gmail.com>
2019-06-06 01:19:39 -0400

    Updated ResNet to work with 2019-06-04 development snapshot.

--- ResNet/Data.swift
+++ ResNet/Data.swift
@@ -35,6 +35,25 @@ func downloadCIFAR10IfNotPresent(to directory: String = ".") {
 struct Example: TensorGroup {
     var label: Tensor<Int32>
     var data: Tensor<Float>
+
+    init(label: Tensor<Int32>, data: Tensor<Float>) {
+        self.label = label
+        self.data = data
+    }
+
+    public init<C: RandomAccessCollection>(
+        _handles: C
+    ) where C.Element: _AnyTensorHandle {
+        precondition(_handles.count == 2)
+        let labelIndex = _handles.startIndex
+        let dataIndex = _handles.index(labelIndex, offsetBy: 1)
+        label = Tensor<Int32>(handle: TensorHandle<Int32>(handle: _handles[labelIndex]))
+        data = Tensor<Float>(handle: TensorHandle<Float>(handle: _handles[dataIndex]))
+    }
+
+    //public var _tensorHandles: [_AnyTensorHandle] { [label.handle.handle, data.handle.handle] }    
+    // error: 'handle' is inaccessible due to 'internal' protection level
+    public var _tensorHandles: [_AnyTensorHandle] { [] }    
 }
 
 // Each CIFAR data file is provided as a Python pickle of NumPy arrays
