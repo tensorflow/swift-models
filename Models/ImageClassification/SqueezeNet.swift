26a8dcc759f7013340d3fbb8b9fa6387ae49cc94
Andr0id100 <snehalranjan5472@gmail.com>
2019-08-08 19:58:52 +0530

    Update SqeezeNet.swift
    Renamed numClasses to classCount and limited all lines to a size of 100.

--- Models/ImageClassification/SqueezeNet.swift
+++ Models/ImageClassification/SqueezeNet.swift
@@ -16,7 +16,8 @@ import TensorFlow
 
 // Original Paper:
 // SqueezeNet: AlexNet Level Accuracy with 50X Fewer Parameters
-// Forrest N. Iandola, Song Han, Matthew W. Moskewicz, Khalid Ashraf, William J. Dally and Kurt Keutzer
+// Forrest N. Iandola, Song Han, Matthew W. Moskewicz, Khalid Ashraf, William J. Dally
+// and Kurt Keutzer
 // https://arxiv.org/pdf/1602.07360.pdf
 
 public struct Fire: Layer {
@@ -24,10 +25,22 @@ public struct Fire: Layer {
     public var expand1: Conv2D<Float>
     public var expand3: Conv2D<Float>
 
-    public init(inputFilterCount: Int, squeezeFilterCount: Int, expand1FilterCount: Int, expand3FilterCount: Int) {
-        squeeze = Conv2D(filterShape: (1, 1, inputFilterCount, squeezeFilterCount), activation: relu)
-        expand1 = Conv2D(filterShape: (1, 1, squeezeFilterCount, expand1FilterCount), activation: relu)
-        expand3 = Conv2D(filterShape: (3, 3, squeezeFilterCount, expand3FilterCount), padding: .same, activation: relu)
+    public init(
+        inputFilterCount: Int,
+        squeezeFilterCount: Int,
+        expand1FilterCount: Int,
+        expand3FilterCount: Int
+    ) {
+        squeeze = Conv2D(
+            filterShape: (1, 1, inputFilterCount, squeezeFilterCount),
+            activation: relu)
+        expand1 = Conv2D(
+            filterShape: (1, 1, squeezeFilterCount, expand1FilterCount),
+            activation: relu)
+        expand3 = Conv2D(
+            filterShape: (3, 3, squeezeFilterCount, expand3FilterCount),
+            padding: .same,
+            activation: relu)
     }
 
     @differentiable
@@ -42,22 +55,54 @@ public struct Fire: Layer {
 public struct SqueezeNet: Layer {
     public var conv1 = Conv2D<Float>(filterShape: (7, 7, 3, 96), strides: (2, 2), padding: .same)
     public var maxPool1 = MaxPool2D<Float>(poolSize: (3, 3), strides: (2, 2))
-    public var fire2 = Fire(inputFilterCount: 96, squeezeFilterCount: 16, expand1FilterCount: 64, expand3FilterCount: 64)
-    public var fire3 = Fire(inputFilterCount: 128, squeezeFilterCount: 16, expand1FilterCount: 64, expand3FilterCount: 64)
-    public var fire4 = Fire(inputFilterCount: 128, squeezeFilterCount: 32, expand1FilterCount: 128, expand3FilterCount: 128)
+    public var fire2 = Fire(
+        inputFilterCount: 96,
+        squeezeFilterCount: 16,
+        expand1FilterCount: 64,
+        expand3FilterCount: 64)
+    public var fire3 = Fire(
+        inputFilterCount: 128,
+        squeezeFilterCount: 16,
+        expand1FilterCount: 64,
+        expand3FilterCount: 64)
+    public var fire4 = Fire(
+        inputFilterCount: 128,
+        squeezeFilterCount: 32,
+        expand1FilterCount: 128,
+        expand3FilterCount: 128)
     public var maxPool4 = MaxPool2D<Float>(poolSize: (3, 3), strides: (2, 2))
-    public var fire5 = Fire(inputFilterCount: 256, squeezeFilterCount: 32, expand1FilterCount: 128, expand3FilterCount: 128)
-    public var fire6 = Fire(inputFilterCount: 256, squeezeFilterCount: 48, expand1FilterCount: 192, expand3FilterCount: 192)
-    public var fire7 = Fire(inputFilterCount: 384, squeezeFilterCount: 48, expand1FilterCount: 192, expand3FilterCount: 192)
-    public var fire8 = Fire(inputFilterCount: 384, squeezeFilterCount: 64, expand1FilterCount: 256, expand3FilterCount: 256)
+    public var fire5 = Fire(
+        inputFilterCount: 256,
+        squeezeFilterCount: 32,
+        expand1FilterCount: 128,
+        expand3FilterCount: 128)
+    public var fire6 = Fire(
+        inputFilterCount: 256,
+        squeezeFilterCount: 48,
+        expand1FilterCount: 192,
+        expand3FilterCount: 192)
+    public var fire7 = Fire(
+        inputFilterCount: 384,
+        squeezeFilterCount: 48,
+        expand1FilterCount: 192,
+        expand3FilterCount: 192)
+    public var fire8 = Fire(
+        inputFilterCount: 384,
+        squeezeFilterCount: 64,
+        expand1FilterCount: 256,
+        expand3FilterCount: 256)
     public var maxPool8 = MaxPool2D<Float>(poolSize: (3, 3), strides: (2, 2))
-    public var fire9 = Fire(inputFilterCount: 512, squeezeFilterCount: 64, expand1FilterCount: 256, expand3FilterCount: 256)
+    public var fire9 = Fire(
+        inputFilterCount: 512,
+        squeezeFilterCount: 64,
+        expand1FilterCount: 256,
+        expand3FilterCount: 256)
     public var conv10: Conv2D<Float>
     public var avgPool10 = AvgPool2D<Float>(poolSize: (13, 13), strides: (1, 1))
     public var dropout = Dropout<Float>(probability: 0.5)
 
-    public init(numClasses: Int) {
-        conv10 = Conv2D(filterShape: (1, 1, 512, numClasses), strides: (1, 1))
+    public init(classCount: Int) {
+        conv10 = Conv2D(filterShape: (1, 1, 512, classCount), strides: (1, 1))
     }
 
     @differentiable
