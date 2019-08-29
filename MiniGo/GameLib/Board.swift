56aca6176bf6624e14c01a451e6873c769c69d5e
Dan Zheng <danielzheng@google.com>
2019-08-28 21:36:07 -0700

    Prettify Go game board.
    Use emojis for stones.

--- MiniGo/GameLib/Board.swift
+++ MiniGo/GameLib/Board.swift
@@ -92,10 +92,12 @@ extension Board: CustomStringConvertible {
             for y in 0..<size {
                 output.append(gapBetweenStones)
                 guard let color = self.color(at: Position(x: x, y: y)) else {
-                    output.append(".")  // Empty position.
+                    // output.append("。")  // Empty position.
+                    output.append("﹒")  // Empty position.
+                    // output.append(".")  // Empty position.
                     continue
                 }
-                output.append(color == .black ? "X" : "O")
+                output.append(color == .black ? "🔵" : "⚪")
             }
             output.append("\n")
         }
