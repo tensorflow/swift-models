a1db3bb4bd0dd5f3428635a027e3fb4b7cf524a6
Brad Larson <bradlarson@google.com>
2019-11-14 07:51:34 -0600

    Initial training and inference benchmarks (#222)
    * Initial implementation of training and inference benchmarks.
    
    * Resolved a typechecker issue.
    
    * Added a Readme.
    
    * Added copyright headers.
    
    * Reworked the benchmarking functions to use a single function that is specialized via enums.
    
    * Reworked the model-specific benchmark into general inference and training cases that can work on arbitrary image classification models.
    
    * Restructured BenchmarkResults to be a plain collection of timings, with specific benchmark processing working on that.
    
    * Rename interpretTimings to interpretedTimings.
    
    Co-Authored-By: Dan Zheng <danielzheng@google.com>
    
    * Change reference to interpretedTimings later.

---
+++ Benchmarks/Benchmark.swift
@@ -0,0 +1,104 @@
+// Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import Foundation
+
+enum BenchmarkVariety {
+    case inferenceThroughput(batches: Int, batchSize: Int)
+    case trainingTime
+}
+
+struct BenchmarkResults {
+    let name: String
+    let iterations: Int
+    let timings: [Double]
+    let variety: BenchmarkVariety
+}
+
+extension BenchmarkResults {
+    var interpretedTimings: [Double] {
+        switch self.variety {
+        case let .inferenceThroughput(batches, batchSize):
+            return timings.map { Double(batches * batchSize) / ($0 / 1000.0) }
+        case .trainingTime:
+            return timings
+        }
+    }
+}
+
+/// Performs the specified benchmark over a certain number of iterations and provides the result to a callback function.
+func benchmark(
+    name: String,
+    iterations: Int, variety: BenchmarkVariety,
+    operation: () -> Void,
+    callback: (BenchmarkResults) -> Void
+) {
+    var timings: [Double] = []
+    for _ in 0..<iterations {
+        let timing = time(operation)
+        timings.append(timing)
+    }
+
+    let results = BenchmarkResults(
+        name: name, iterations: iterations,
+        timings: timings, variety: variety
+    )
+    callback(results)
+}
+
+/// Returns the time elapsed while running `body` in milliseconds.
+func time(_ body: () -> Void) -> Double {
+    let divisor: Double = 1_000_000
+    let start = Double(DispatchTime.now().uptimeNanoseconds) / divisor
+    body()
+    let end = Double(DispatchTime.now().uptimeNanoseconds) / divisor
+    let elapsed = end - start
+    return elapsed
+}
+
+/// Provides the average and standard deviation of an array of values.
+func statistics(for values: [Double]) -> (average: Double, standardDeviation: Double) {
+    guard values.count > 0 else { return (average: 0.0, standardDeviation: 0.0) }
+    guard values.count > 1 else { return (average: values.first!, standardDeviation: 0.0) }
+
+    let average = (values.reduce(0.0) { $0 + $1 }) / Double(values.count)
+
+    let standardDeviation = sqrt(
+        values.reduce(0.0) { $0 + ($1 - average) * ($1 - average) }
+            / Double(values.count - 1))
+
+    return (average: average, standardDeviation: standardDeviation)
+}
+
+// This is a simple callback function example that only logs the result to the console.
+func logResults(_ result: BenchmarkResults) {
+    let (average, standardDeviation) = statistics(for: result.interpretedTimings)
+
+    switch result.variety {
+    case .inferenceThroughput:
+        print(
+            """
+                Benchmark: \(result.name):
+                \tAfter \(result.iterations) iterations:
+                \tSamples per second: \(String(format: "%.2f", average)), standard deviation: \(String(format: "%.2f", standardDeviation))
+            """)
+    case .trainingTime:
+        print(
+            """
+                Benchmark: \(result.name):
+                \tAfter \(result.iterations) iterations:
+                \tAverage: \(String(format: "%.2f", average)) ms, standard deviation: \(String(format: "%.2f", standardDeviation)) ms
+            """)
+    }
+}
