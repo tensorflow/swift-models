fed809716006297438802878c456319584faa74e
Xihui Wu <xhwu@google.com>
2019-12-06 13:41:22 -0800

    More address in CIFAR10Tests.swift

--- Tests/DatasetsTests/CIFAR10/CIFAR10Tests.swift
+++ Tests/DatasetsTests/CIFAR10/CIFAR10Tests.swift
@@ -1,18 +1,18 @@
 import TensorFlow
 import XCTest
-
-@testable import Datasets
+import Datasets
 
 final class CIFAR10Tests: XCTestCase {
-	func testCreateCIFAR10() {
-		let dataset = CIFAR10()
+    func testCreateCIFAR10() {
+        let dataset = CIFAR10()
 
-		var totalCount = 0
-		for example in dataset.trainingDataset {
-	    	XCTAssertTrue((0..<10).contains(example.label.scalar!))
-	    	XCTAssertEqual(example.data.shape, [32, 32, 3])
-			totalCount += 1
-		}
-		XCTAssertEqual(totalCount, 50000)
-	}
+        var totalCount = 0
+        for example in dataset.trainingDataset {
+            XCTAssertTrue((0..<10).contains(example.label.scalar!))
+            XCTAssertEqual(example.data.shape, [32, 32, 3])
+            totalCount += 1
+        }
+        XCTAssertEqual(totalCount, 50000)
+    }
 }
+
