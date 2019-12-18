59c15f2e5221fb56d6ba0a10ac81c04ad8bda560
brett koonce <koonce@gmail.com>
2019-12-18 11:06:04 -0600

    rewrite vgg to use blocks, add vgg 19 (#251)
    * rewrite vgg to use blocks, add vgg 19
    
    * refactor/simplify init logic
    
    * redo tests

--- Models/ImageClassification/VGG.swift
+++ Models/ImageClassification/VGG.swift
@@ -19,44 +19,79 @@ import TensorFlow
 // Karen Simonyan, Andrew Zisserman
 // https://arxiv.org/abs/1409.1556
 
+public struct VGGBlock: Layer {
+    var blocks: [Conv2D<Float>] = []
+    var maxpool = MaxPool2D<Float>(poolSize: (2, 2), strides: (2, 2))
+
+    public init(featureCounts: (Int, Int, Int, Int), blockCount: Int) {
+        self.blocks = [Conv2D<Float>(filterShape: (3, 3, featureCounts.0, featureCounts.1),
+            padding: .same,
+            activation: relu)]
+        for _ in 1..<blockCount {
+            self.blocks += [Conv2D<Float>(filterShape: (3, 3, featureCounts.2, featureCounts.3),
+                padding: .same,
+                activation: relu)]
+        }
+    }
+
+    @differentiable
+    public func callAsFunction(_ input: Tensor<Float>) -> Tensor<Float> {
+        return maxpool(blocks.differentiableReduce(input) { $1($0) })
+    }
+}
+
 public struct VGG16: Layer {
-    var conv1a = Conv2D<Float>(filterShape: (3, 3, 3, 64), padding: .same, activation: relu)
-    var conv1b = Conv2D<Float>(filterShape: (3, 3, 64, 64), padding: .same, activation: relu)
-    var pool1 = MaxPool2D<Float>(poolSize: (2, 2), strides: (2, 2))
-
-    var conv2a = Conv2D<Float>(filterShape: (3, 3, 64, 128), padding: .same, activation: relu)
-    var conv2b = Conv2D<Float>(filterShape: (3, 3, 128, 128), padding: .same, activation: relu)
-    var pool2 = MaxPool2D<Float>(poolSize: (2, 2), strides: (2, 2))
-
-    var conv3a = Conv2D<Float>(filterShape: (3, 3, 128, 256), padding: .same, activation: relu)
-    var conv3b = Conv2D<Float>(filterShape: (3, 3, 256, 256), padding: .same, activation: relu)
-    var conv3c = Conv2D<Float>(filterShape: (3, 3, 256, 256), padding: .same, activation: relu)
-    var pool3 = MaxPool2D<Float>(poolSize: (2, 2), strides: (2, 2))
-
-    var conv4a = Conv2D<Float>(filterShape: (3, 3, 256, 512), padding: .same, activation: relu)
-    var conv4b = Conv2D<Float>(filterShape: (3, 3, 512, 512), padding: .same, activation: relu)
-    var conv4c = Conv2D<Float>(filterShape: (3, 3, 512, 512), padding: .same, activation: relu)
-    var pool4 = MaxPool2D<Float>(poolSize: (2, 2), strides: (2, 2))
-
-    var conv5a = Conv2D<Float>(filterShape: (3, 3, 512, 512), padding: .same, activation: relu)
-    var conv5b = Conv2D<Float>(filterShape: (3, 3, 512, 512), padding: .same, activation: relu)
-    var conv5c = Conv2D<Float>(filterShape: (3, 3, 512, 512), padding: .same, activation: relu)
-    var pool5 = MaxPool2D<Float>(poolSize: (2, 2), strides: (2, 2))
+    var layer1: VGGBlock
+    var layer2: VGGBlock
+    var layer3: VGGBlock
+    var layer4: VGGBlock
+    var layer5: VGGBlock
 
     var flatten = Flatten<Float>()
     var dense1 = Dense<Float>(inputSize: 512 * 7 * 7, outputSize: 4096, activation: relu)
     var dense2 = Dense<Float>(inputSize: 4096, outputSize: 4096, activation: relu)
-    var dense3 = Dense<Float>(inputSize: 4096, outputSize: 1000, activation: softmax)
+    var output: Dense<Float>
 
-    public init() {}
+    public init(classCount: Int = 1000) {
+        layer1 = VGGBlock(featureCounts: (3, 64, 64, 64), blockCount: 2)
+        layer2 = VGGBlock(featureCounts: (64, 128, 128, 128), blockCount: 2)
+        layer3 = VGGBlock(featureCounts: (128, 256, 256, 256), blockCount: 3)
+        layer4 = VGGBlock(featureCounts: (256, 512, 512, 512), blockCount: 3)
+        layer5 = VGGBlock(featureCounts: (512, 512, 512, 512), blockCount: 3)
+        output = Dense(inputSize: 4096, outputSize: classCount, activation: softmax)
+    }
 
     @differentiable
     public func callAsFunction(_ input: Tensor<Float>) -> Tensor<Float> {
-        let conv1 = input.sequenced(through: conv1a, conv1b, pool1)
-        let conv2 = conv1.sequenced(through: conv2a, conv2b, pool2)
-        let conv3 = conv2.sequenced(through: conv3a, conv3b, conv3c, pool3)
-        let conv4 = conv3.sequenced(through: conv4a, conv4b, conv4c, pool4)
-        let conv5 = conv4.sequenced(through: conv5a, conv5b, conv5c, pool5)
-        return conv5.sequenced(through: flatten, dense1, dense2, dense3)
+        let backbone = input.sequenced(through: layer1, layer2, layer3, layer4, layer5)
+        return backbone.sequenced(through: flatten, dense1, dense2, output)
+    }
+}
+
+public struct VGG19: Layer {
+    var layer1: VGGBlock
+    var layer2: VGGBlock
+    var layer3: VGGBlock
+    var layer4: VGGBlock
+    var layer5: VGGBlock
+
+    var flatten = Flatten<Float>()
+    var dense1 = Dense<Float>(inputSize: 512 * 7 * 7, outputSize: 4096, activation: relu)
+    var dense2 = Dense<Float>(inputSize: 4096, outputSize: 4096, activation: relu)
+    var output: Dense<Float>
+
+    public init(classCount: Int = 1000) {
+        layer1 = VGGBlock(featureCounts: (3, 64, 64, 64), blockCount: 2)
+        layer2 = VGGBlock(featureCounts: (64, 128, 128, 128), blockCount: 2)
+        layer3 = VGGBlock(featureCounts: (128, 256, 256, 256), blockCount: 4)
+        layer4 = VGGBlock(featureCounts: (256, 512, 512, 512), blockCount: 4)
+        layer5 = VGGBlock(featureCounts: (512, 512, 512, 512), blockCount: 4)
+        output = Dense(inputSize: 4096, outputSize: classCount, activation: softmax)
+    }
+
+    @differentiable
+    public func callAsFunction(_ input: Tensor<Float>) -> Tensor<Float> {
+        let backbone = input.sequenced(through: layer1, layer2, layer3, layer4, layer5)
+        return backbone.sequenced(through: flatten, dense1, dense2, output)
     }
 }
