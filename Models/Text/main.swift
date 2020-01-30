9a00c049cca1c4a0d1e40d99c42b14f24c538eff
Anthony Platanios <e.a.platanios@gmail.com>
2020-01-30 02:52:07 -0500

    Made some minor edits to get the BERT classifier training to work for CoLA. (#293)

--- Models/Text/main.swift
+++ Models/Text/main.swift
@@ -4,15 +4,21 @@ import Foundation
 let bertPretrained = BERT.PreTrainedModel.bertBase(cased: false, multilingual: false)
 let workspaceURL = URL(fileURLWithPath: "/tmp/bert_models", isDirectory: true)
 let bert = try BERT.PreTrainedModel.load(bertPretrained)(from: workspaceURL)
-var bertClassifier = BERTClassifier(bert: bert, classCount: 2)
+var bertClassifier = BERTClassifier(bert: bert, classCount: 1)
 
-var colaTask = try CoLA(for: bertClassifier, taskDirectoryURL: workspaceURL, maxSequenceLength: 50, batchSize: 32)
+var colaTask = try CoLA(
+  for: bertClassifier,
+  taskDirectoryURL: workspaceURL,
+  maxSequenceLength: 512,
+  batchSize: 32)
 
-let lr = FixedParameter<Float>(2e-5)
-let pivotStepCount: UInt64 = 1000
-let lr2 = LinearlyWarmedUpParameter(baseParameter: lr, warmUpStepCount: pivotStepCount, warmUpOffset: 0)
-let lr3 =  LinearlyDecayedParameter(baseParameter: lr2, slope: 3, startStep: pivotStepCount)
-var optimizer = WeightDecayedAdam(for: bertClassifier, learningRate: lr3, maxGradientGlobalNorm: 1)
+var optimizer = WeightDecayedAdam(
+  for: bertClassifier,
+  learningRate: LinearlyDecayedParameter(
+    baseParameter: FixedParameter<Float>(2e-5),
+    slope: -2e-8, // The LR decays linearly to zero in 1000 steps.
+    startStep: 0),
+  maxGradientGlobalNorm: 1)
 
 print("Training BERT for the CoLA task!")
 let epochCount = 1000
