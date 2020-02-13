9606e6589a90758816908765462ddb0d7f5c9ae3
Jacopo Mangiavacchi <jamangia@microsoft.com>
2020-02-12 19:33:56 -0800

    Add Training

--- Tabular/main.swift
+++ Tabular/main.swift
@@ -15,8 +15,71 @@
 import Foundation
 import TensorFlow
 
+// Import Dataset
 let dataset = BostonHousingDataset()
 
-print(dataset.numRecords)
+// Create Model
+struct RegressionModel: Layer {
+    var layer1 = Dense<Float>(inputSize: 13, outputSize: 64, activation: relu)
+    var layer2 = Dense<Float>(inputSize: 64, outputSize: 32, activation: relu)
+    var layer3 = Dense<Float>(inputSize: 32, outputSize: 1)
+    
+    @differentiable
+    func callAsFunction(_ input: Tensor<Float>) -> Tensor<Float> {
+        return input.sequenced(through: layer1, layer2, layer3)
+    }
+}
 
+var model = RegressionModel()
+
+//Train Model
+let optimizer = RMSProp(for: model, learningRate: 0.001)
+Context.local.learningPhase = .training
+
+let epochCount = 500
+let batchSize = 32
+let numberOfBatch = Int(ceil(Double(dataset.numTrainRecords) / Double(batchSize)))
+let shuffle = true
+
+func mae(predictions: Tensor<Float>, truths: Tensor<Float>) -> Float {
+    return abs(Tensor<Float>(predictions - truths)).mean().scalarized()
+}
+
+print("Starting training...")
+
+for _ in 1...epochCount {
+    var epochLoss: Float = 0
+    var epochMAE: Float = 0
+    var batchCount: Int = 0
+    var batchArray = Array(repeating: false, count: numberOfBatch)
+    for batch in 0..<numberOfBatch {
+        var r = batch
+        if shuffle {
+            while true {
+                r = Int.random(in: 0..<numberOfBatch)
+                if !batchArray[r] {
+                    batchArray[r] = true
+                    break
+                }
+            }
+        }
+        
+        let batchStart = r * batchSize
+        let batchEnd = min(dataset.numTrainRecords, batchStart + batchSize)
+        let (loss, grad) = model.valueWithGradient { (model: RegressionModel) -> Tensor<Float> in
+            let logits = model(dataset.xTrain[batchStart..<batchEnd])
+            return meanSquaredError(predicted: logits, expected: dataset.yTrain[batchStart..<batchEnd])
+        }
+        optimizer.update(&model, along: grad)
+        
+        let logits = model(dataset.xTrain[batchStart..<batchEnd])
+        epochMAE += mae(predictions: logits, truths: dataset.yTrain[batchStart..<batchEnd])
+        epochLoss += loss.scalarized()
+        batchCount += 1
+    }
+    epochMAE /= Float(batchCount)
+    epochLoss /= Float(batchCount)
+
+    // print("Epoch \(epoch): MSE: \(epochLoss), MAE: \(epochMAE)")
+}
 
