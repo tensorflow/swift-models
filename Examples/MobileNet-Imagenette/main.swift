3810d961a0e98f90f8c72537619d6c9529115ca2
Sylvain Gugger <sylvain.gugger@gmail.com>
2020-02-14 14:01:47 -0800

    Change mobilenet example

--- Examples/MobileNet-Imagenette/main.swift
+++ Examples/MobileNet-Imagenette/main.swift
@@ -16,10 +16,12 @@ import Datasets
 import ImageClassificationModels
 import TensorFlow
 
-let batchSize = 16
-
-let dataset = Imagenette(inputSize: .resized320, outputSize: 224)
-let testBatches = dataset.testDataset.batched(batchSize)
+let batchers = ImagenetteBatchers(
+    inputSize: .resized320, 
+    outputSize: 224, 
+    batchSize: 16,
+    numWorkers: 4
+)
 
 var model = MobileNetV1(classCount: 10)
 
@@ -31,9 +33,7 @@ for epoch in 1...10 {
     Context.local.learningPhase = .training
     var trainingLossSum: Float = 0
     var trainingBatchCount = 0
-    let trainingShuffled = dataset.trainingDataset.shuffled(
-        sampleCount: dataset.trainingExampleCount, randomSeed: Int64(epoch))
-    for batch in trainingShuffled.batched(batchSize) {
+    for batch in batchers.training.sequenced() {
         let (labels, images) = (batch.label, batch.data)
         let (loss, gradients) = valueWithGradient(at: model) { model -> Tensor<Float> in
             let logits = model(images)
@@ -49,7 +49,7 @@ for epoch in 1...10 {
     var testBatchCount = 0
     var correctGuessCount = 0
     var totalGuessCount = 0
-    for batch in testBatches {
+    for batch in batchers.validation.sequenced() {
         let (labels, images) = (batch.label, batch.data)
         let logits = model(images)
         testLossSum += softmaxCrossEntropy(logits: logits, labels: labels).scalarized()
