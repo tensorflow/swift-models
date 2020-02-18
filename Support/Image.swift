72518132a72ea7b380a4a91d65c0e27f13088181
Rahul Bhalley <rahulbhalley@protonmail.com>
2020-02-19 02:06:07 +0530

    Activates resizing and makes it more flexible from user viewpoint.

--- Support/Image.swift
+++ Support/Image.swift
@@ -1,4 +1,4 @@
-// Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+// Copyright 2020 The TensorFlow Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -20,32 +20,32 @@ public struct Image {
         case bgr
         case rgb
     }
-
+    
     enum ImageTensor {
         case float(data: Tensor<Float>)
         case uint8(data: Tensor<UInt8>)
     }
-
+    
     let imageData: ImageTensor
-
+    
     public var tensor: Tensor<Float> {
         switch self.imageData {
         case let .float(data): return data
         case let .uint8(data): return Tensor<Float>(data)
         }
     }
-
+    
     public init(tensor: Tensor<UInt8>) {
         self.imageData = .uint8(data: tensor)
     }
-
+    
     public init(tensor: Tensor<Float>) {
         self.imageData = .float(data: tensor)
     }
-
+    
     public init(jpeg url: URL, byteOrdering: ByteOrdering = .rgb) {
-        let loadedFile = _Raw.readFile(filename: StringTensor(url.absoluteString))
-        let loadedJpeg = _Raw.decodeJpeg(contents: loadedFile, channels: 3, dctMethod: "")
+        let loadedData = _Raw.readFile(filename: StringTensor(url.absoluteString))
+        let loadedJpeg = _Raw.decodeJpeg(contents: loadedData, channels: 3, dctMethod: "")
         if byteOrdering == .bgr {
             self.imageData = .uint8(
                 data: _Raw.reverse(loadedJpeg, dims: Tensor<Bool>([false, false, false, true])))
@@ -53,8 +53,8 @@ public struct Image {
             self.imageData = .uint8(data: loadedJpeg)
         }
     }
-
-    public func save(to url: URL, format: _Raw.Format = .grayscale, quality: Int64 = 95) {
+    
+    public func save(to url: URL, format: _Raw.Format = .rgb, quality: Int64 = 95) {
         let outputImageData: Tensor<UInt8>
         switch format {
         case .grayscale:
@@ -77,35 +77,35 @@ public struct Image {
             print("Image saving isn't supported for the format \(format).")
             exit(-1)
         }
-
+        
         let encodedJpeg = _Raw.encodeJpeg(
             image: outputImageData, format: format, quality: quality, xmpMetadata: "")
         _Raw.writeFile(filename: StringTensor(url.absoluteString), contents: encodedJpeg)
     }
-
+    
     public func resized(to size: (Int, Int)) -> Image {
         switch self.imageData {
         case let .uint8(data):
             return Image(
                 tensor: _Raw.resizeBilinear(
-                    images: Tensor<UInt8>([data]),
-                    size: Tensor<Int32>([Int32(size.0), Int32(size.1)])))
+                    images: Tensor<UInt8>(data.rankLifted()),
+                    size: Tensor<Int32>([Int32(size.0), Int32(size.1)])).squeezingShape(at: 0))
         case let .float(data):
             return Image(
                 tensor: _Raw.resizeBilinear(
-                    images: Tensor<Float>([data]),
-                    size: Tensor<Int32>([Int32(size.0), Int32(size.1)])))
+                    images: Tensor<Float>(data.rankLifted()),
+                    size: Tensor<Int32>([Int32(size.0), Int32(size.1)])).squeezingShape(at: 0))
         }
 
     }
 }
 
-public func saveImage(_ tensor: Tensor<Float>, size: (Int, Int), directory: String, name: String)
-    throws
-{
+public func saveImage(_ tensor: Tensor<Float>, size: (Int?, Int?) = (nil, nil),
+                      format: _Raw.Format = .rgb, directory: String, name: String,
+                      quality: Int64 = 95) throws {
     try createDirectoryIfMissing(at: directory)
-    let reshapedTensor = tensor.reshaped(to: [size.0, size.1, 1])
-    let image = Image(tensor: reshapedTensor)
+    let image = Image(tensor: tensor)
+    let resizedImage = size.0 != nil && size.1 != nil ? image.resized(to: (size.0!, size.1!)) : image
     let outputURL = URL(fileURLWithPath: "\(directory)\(name).jpg")
-    image.save(to: outputURL)
+    resizedImage.save(to: outputURL, format: format, quality: quality)
 }
