021d54c421d576d817bde8b17ba7bdd8a7184cea
Michelle Casbon <michellecasbon@google.com>
2020-03-06 23:08:31 +0000

    Make functions static members of WikiText2

--- Datasets/WikiText2/WikiText2.swift
+++ Datasets/WikiText2/WikiText2.swift
@@ -25,7 +25,7 @@ import TensorFlow
 public struct WikiText2 {
     public let trainingDataset: LanguageModelDataset<[Int]>
     public let validationDataset: LanguageModelDataset<[Int]>
-    let bpe: BytePairEncoder
+    public let bpe: BytePairEncoder
 
     public init(bpe: BytePairEncoder) {
         self.init(
@@ -35,80 +35,80 @@ public struct WikiText2 {
 
     public init(localStorageDirectory: URL, bpe: BytePairEncoder) {
         do {
-            self.trainingDataset = try loadWikiText2Training(
+            self.trainingDataset = try WikiText2.loadWikiText2Training(
                     localStorageDirectory: localStorageDirectory, bpe: bpe)
-            self.validationDataset = try loadWikiText2Validation(
+            self.validationDataset = try WikiText2.loadWikiText2Validation(
                     localStorageDirectory: localStorageDirectory, bpe: bpe)
             self.bpe = bpe
         } catch {
             fatalError("Could not load WikiText2 dataset: \(error)")
         }
     }
+
+    private static func downloadWikiText2IfNotPresent(to directory: URL) {
+        let downloadPath = directory.appendingPathComponent("wikitext-2").path
+        let directoryExists = FileManager.default.fileExists(atPath: downloadPath)
+        let contentsOfDir = try? FileManager.default.contentsOfDirectory(atPath: downloadPath)
+        let directoryEmpty = (contentsOfDir == nil) || (contentsOfDir!.isEmpty)
+
+        guard !directoryExists || directoryEmpty else { return }
+
+        let location = URL(
+            string: "https://s3.amazonaws.com/fast-ai-nlp/")!
+        let _ = DatasetUtilities.downloadResource(
+            filename: "wikitext-2", fileExtension: "tgz",
+            remoteRoot: location, localStorageDirectory: directory)
+    }
+
+    private static func readCSV(in file: URL) throws -> [String] {
+        let rawText = try! String(contentsOf: file, encoding: .utf8)
+        var rows = rawText.components(separatedBy: "\"\n\"")
+        //Removing the initial "
+        rows[0] = String(rows[0].dropFirst())
+        //Removing the last "\n
+        rows[rows.count-1] = String(rows[rows.count-1].substring(to: rows[rows.count-1].count-2))
+        return rows
+    }
+
+    private static func embedding(for string: String, bpe: BytePairEncoder) -> [Int] {
+        let tokens = bpe.encode(token: string, variant: .gpt2)
+        // TODO(michellecasbon): Decide how to prevent OOV or choose a better ID (probably not 0).
+        let ids = tokens.map { bpe.vocabulary.id(forToken: $0) ?? 0 }
+        return ids
+    }
+
+    private static func loadWikiText2Directory(
+        named name: String, in directory: URL,
+        bpe: BytePairEncoder) throws -> LanguageModelDataset<[Int]> {
+        downloadWikiText2IfNotPresent(to: directory)
+        let path = directory.appendingPathComponent("wikitext-2/\(name).csv")
+
+        let documentsFull = try readCSV(in: path)
+        // TODO(michellecasbon): Process a larger number of documents.
+        let documents = Array(documentsFull[0..<1])
+
+        let embeddings = documents.map{ embedding(for: $0, bpe: bpe) }
+        let lengths = embeddings.map{ $0.count }
+
+        return LanguageModelDataset(
+            batchSize: 4,
+            sequenceLength: 64,
+            items: embeddings,
+            lengths: lengths
+        )
+    }
+
+    private static func loadWikiText2Training(localStorageDirectory: URL, bpe: BytePairEncoder) throws
+        -> LanguageModelDataset<[Int]>
+    {
+        return try loadWikiText2Directory(
+            named: "train", in: localStorageDirectory, bpe: bpe)
+    }
+
+    private static func loadWikiText2Validation(localStorageDirectory: URL, bpe: BytePairEncoder) throws
+        -> LanguageModelDataset<[Int]>
+    {
+        return try loadWikiText2Directory(
+            named: "test", in: localStorageDirectory, bpe: bpe)
+    }
 }
-
-func downloadWikiText2IfNotPresent(to directory: URL) {
-    let downloadPath = directory.appendingPathComponent("wikitext-2").path
-    let directoryExists = FileManager.default.fileExists(atPath: downloadPath)
-    let contentsOfDir = try? FileManager.default.contentsOfDirectory(atPath: downloadPath)
-    let directoryEmpty = (contentsOfDir == nil) || (contentsOfDir!.isEmpty)
-
-    guard !directoryExists || directoryEmpty else { return }
-
-    let location = URL(
-        string: "https://s3.amazonaws.com/fast-ai-nlp/")!
-    let _ = DatasetUtilities.downloadResource(
-        filename: "wikitext-2", fileExtension: "tgz",
-        remoteRoot: location, localStorageDirectory: directory)
-}
-
-func readCSV(in file: URL) throws -> [String] {
-    let rawText = try! String(contentsOf: file, encoding: .utf8)
-    var rows = rawText.components(separatedBy: "\"\n\"")
-    //Removing the initial "
-    rows[0] = String(rows[0].dropFirst())
-    //Removing the last "\n
-    rows[rows.count-1] = String(rows[rows.count-1].substring(to: rows[rows.count-1].count-2))
-    return rows
-}
-
-func embedding(for string: String, bpe: BytePairEncoder) -> [Int] {
-    let tokens = bpe.encode(token: string, variant: .gpt2)
-    // TODO(michellecasbon): Decide how to prevent OOV or choose a better ID (probably not 0).
-    let ids = tokens.map { bpe.vocabulary.id(forToken: $0) ?? 0 }
-    return ids
-}
-
-func loadWikiText2Directory(
-    named name: String, in directory: URL, bpe: BytePairEncoder) throws -> LanguageModelDataset<[Int]> {
-    downloadWikiText2IfNotPresent(to: directory)
-    let path = directory.appendingPathComponent("wikitext-2/\(name).csv")
-
-    let documentsFull = try readCSV(in: path)
-    // TODO(michellecasbon): Process a larger number of documents.
-    let documents = Array(documentsFull[0..<1])
-
-    let embeddings = documents.map{ embedding(for: $0, bpe: bpe) }
-    let lengths = embeddings.map{ $0.count }
-
-    return LanguageModelDataset(
-        batchSize: 4,
-        sequenceLength: 64,
-        items: embeddings,
-        lengths: lengths
-    )
-}
-
-func loadWikiText2Training(localStorageDirectory: URL, bpe: BytePairEncoder) throws
-    -> LanguageModelDataset<[Int]>
-{
-    return try loadWikiText2Directory(
-        named: "train", in: localStorageDirectory, bpe: bpe)
-}
-
-func loadWikiText2Validation(localStorageDirectory: URL, bpe: BytePairEncoder) throws
-    -> LanguageModelDataset<[Int]>
-{
-    return try loadWikiText2Directory(
-        named: "test", in: localStorageDirectory, bpe: bpe)
-}
-
