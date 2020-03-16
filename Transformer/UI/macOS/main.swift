e9775347a5a517ffaf91cd16f34d8f0dc6c7c77a
Brad Larson <bradlarson@google.com>
2020-03-16 17:59:38 -0500

    Add the conditional compilation just for macOS.

--- Transformer/UI/macOS/main.swift
+++ Transformer/UI/macOS/main.swift
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#if canImport(AppKit)
 import AppKit
 
 import Dispatch
@@ -189,3 +190,8 @@ extension SwiftApplicationDelegate {
 let delegate: SwiftApplicationDelegate = SwiftApplicationDelegate()
 NSApplication.shared.delegate = delegate
 _ = NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv)
+
+#else
+fatalError("This demo requires AppKit on macOS to run.")
+#endif
+
