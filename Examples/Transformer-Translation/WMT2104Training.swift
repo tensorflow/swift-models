c92bbdd19c5a3c0c8f7284dd814f83fe58d3630f
Andre Carrera <acarrera94@gmail.com>
2020-03-27 11:58:41 -0600

    fixing encoding

--- Examples/Transformer-Translation/WMT2104Training.swift
+++ Examples/Transformer-Translation/WMT2104Training.swift
@@ -18,6 +18,8 @@ public struct TextProcessor {
     private let targetPadId: Int32
     private let bosId: Int32
     private let eosId: Int32
+    private let targetUnkId: Int32
+    private let sourceUnkId: Int32
     private let maxSequenceLength: Int
     private let batchSize: Int
     public init(tokenizer: Tokenizer, sourceVocabulary: Vocabulary, targetVocabulary: Vocabulary, maxSequenceLength: Int,
@@ -32,6 +34,8 @@ public struct TextProcessor {
         self.targetPadId = Int32(self.targetVocabulary.id(forToken: BLANK_WORD)!)
         self.bosId = Int32(self.targetVocabulary.id(forToken: BOS_WORD)!)
         self.eosId = Int32(self.targetVocabulary.id(forToken: EOS_WORD)!)
+        self.sourceUnkId = Int32(self.sourceVocabulary.id(forToken: UNKNOWN_WORD)!)
+        self.targetUnkId = Int32(self.targetVocabulary.id(forToken: UNKNOWN_WORD)!)
     }
     // This will take all source and target sequenes
     // return batches where each batch is based on the size targets in the sequence.
@@ -71,16 +75,16 @@ public struct TextProcessor {
 //
 //        return groupedBySourceSize
 //    }
-    
+    /// only pads target sequence to max sequence length,
     public func preprocess(example: WMT2014EnDe.Example) -> TranslationBatch {
         
         let encodedSource = self.tokenizer.tokenize(example.sourceSentence)
             .prefix(self.maxSequenceLength)
-            .map{ Int32(self.sourceVocabulary.id(forToken: $0) ?? 0)}
+            .map{ Int32(self.sourceVocabulary.id(forToken: $0) ?? Int(self.sourceUnkId))}
         
         var encodedTarget = self.tokenizer.tokenize(example.targetSentence)
             .prefix(self.maxSequenceLength - 2)
-            .map{ Int32(self.targetVocabulary.id(forToken: $0) ?? 0)}
+            .map{ Int32(self.targetVocabulary.id(forToken: $0) ?? Int(self.targetUnkId))}
         encodedTarget = [bosId] + encodedTarget + [eosId]
         let paddingCount = encodedTarget.count < maxSequenceLength ? maxSequenceLength - encodedTarget.count : 0
         let padding = [Int32](repeating: targetPadId, count: paddingCount)
@@ -94,6 +98,15 @@ public struct TextProcessor {
         // padding is going to be equal to the difference between maxSequence length and the totalEncod
         let targetTensor = Tensor<Int32>.init( encodedTarget).expandingShape(at: 0)
         let singleBatch = TranslationBatch(source: sourceTensor, target: targetTensor, sourcePadId: sourcePadId, targetPadId: targetPadId)
+        
+//        print("original source:", example.sourceSentence)
+//        print("decoded source:", decode(tensor: singleBatch.tokenIds, vocab: sourceVocabulary))
+//        
+//        print("max len = \(maxSequenceLength)")
+//        print("encoded target \(encodedTarget.count) last: \(encodedTarget.last!)")
+//        print("original target:", example.targetSentence)
+//        print("decoded target:", decode(tensor: singleBatch.targetTokenIds, vocab: targetVocabulary))
+//        print("decoded truth:", decode(tensor: singleBatch.targetTruth, vocab: targetVocabulary))
         return singleBatch
     }
     
@@ -103,16 +116,18 @@ func decode(tensor: Tensor<Int32>, vocab: Vocabulary) -> String {
   let endId = Int32(vocab.id(forToken: "</s>")!)
    var words = [String]()
    for scalar in tensor.scalars {
-       if Int(scalar) == endId {
-           break
-       } else if let token = vocab.token(forId: Int(scalar)) {
+//       if Int(scalar) == endId {
+//           break
+//       } else
+        if let token = vocab.token(forId: Int(scalar)) {
            words.append(token)
        }
    }
    return words.joined(separator: " ")
 }
 
-extension Vocabulary {    
+extension Vocabulary {
+    
     public init(fromFile fileURL: URL, specialTokens: [String]) throws {
         let vocabItems = try ( String(contentsOfFile: fileURL.path, encoding: .utf8))
         .components(separatedBy: .newlines)
@@ -121,7 +136,7 @@ extension Vocabulary {
                 (specialTokens + vocabItems)
                 .filter { $0.count > 0 }
                 .enumerated().map { ($0.element, $0.offset) },
-            uniquingKeysWith: { (v1, v2) in min(v1, v2) })
+            uniquingKeysWith: { (v1, v2) in max(v1, v2) })
         self.init(tokensToIds: dictionary )
     }
 }
