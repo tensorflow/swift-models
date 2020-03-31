86737cedec60d9180d568e7c495a3855fab065d7
brett koonce <koonce@gmail.com>
2020-03-30 23:11:49 -0500

    swap hard sigmoid back in

--- Models/ImageClassification/MobileNetV3.swift
+++ Models/ImageClassification/MobileNetV3.swift
@@ -195,7 +195,7 @@ public struct InvertedResidualBlock: Layer {
                 input.shape[0], 1, 1, self.hiddenDimension
             ])
             squeezeExcite = depthwise
-                * sigmoid(seExpandConv(relu(seReduceConv(seAvgPoolReshaped))))
+                * hardSigmoid(seExpandConv(relu(seReduceConv(seAvgPoolReshaped))))
         } else {
             squeezeExcite = depthwise
         }
