fe71be0b99f95d3fb6e6a65a6b2bb0d4e1094519
Denys Shabalin <shabalin@google.com>
2020-04-08 13:47:24 +0200

    Initial prototype of typed codable metadata parsing

--- Datasets/Coco/COCOMetadata.swift
+++ Datasets/Coco/COCOMetadata.swift
@@ -30,13 +30,13 @@ public struct COCOLicense: Decodable {
     }
 }
 public struct COCOImage: Decodable {
-    var license: Int
+    var license: Int?
     var file_name: String
-    var coco_url: String
+    var coco_url: String?
     var height: Int
     var width: Int
-    var date_captured: String
-    var flickr_url: String
+    var date_captured: String?
+    var flickr_url: String?
     var id: Int
     public init(
         license: Int, file_name: String, coco_url: String, height: Int, width: Int,
@@ -52,30 +52,10 @@ public struct COCOImage: Decodable {
         self.id = id
     }
 }
-public struct COCOSize: Decodable {
-    var _1: Int
-    var _2: Int
-    public init(_1: Int, _2: Int) {
-        self._1 = _1
-        self._2 = _2
-    }
-}
-public struct COCOBoundingBox: Decodable {
-    var _1: Float
-    var _2: Float
-    var _3: Float
-    var _4: Float
-    public init(_1: Float, _2: Float, _3: Float, _4: Float) {
-        self._1 = _1
-        self._2 = _2
-        self._3 = _3
-        self._4 = _4
-    }
-}
 public enum COCOSegmentation {
     case polygon(values: [[Float]])
-    case uncompressedRLE(counts: [Int], size: COCOSize)
-    case compressedRLE(counts: String, size: COCOSize)
+    case uncompressedRLE(counts: [Int], size: [Int])
+    case compressedRLE(counts: String, size: [Int])
 }
 extension COCOSegmentation: Decodable {
     // TODO: implement decoder
@@ -86,18 +66,18 @@ extension COCOSegmentation: Decodable {
 public struct COCOAnnotation: Decodable {
     var image_id: Int
     var id: Int
-    var caption: String
-    var segmentation: COCOSegmentation
-    var area: Float
-    var iscrowd: Int
-    var bbox: COCOBoundingBox
-    var category_id: Int
-    var ignore: Int
-    var num_keypoints: Int
-    var keypoints: [Int]
+    var caption: String?
+    var segmentation: COCOSegmentation?
+    var area: Float?
+    var iscrowd: Int?
+    var bbox: [Float]?
+    var category_id: Int?
+    var ignore: Int?
+    var num_keypoints: Int?
+    var keypoints: [Int]?
     public init(
         image_id: Int, id: Int, caption: String, segmentation: COCOSegmentation, area: Float,
-        iscrowd: Int, bbox: COCOBoundingBox, category_id: Int, ignore: Int, num_keypoints: Int,
+        iscrowd: Int, bbox: [Float], category_id: Int, ignore: Int, num_keypoints: Int,
         keypoints: [Int]
     ) {
         self.image_id = image_id
@@ -117,10 +97,10 @@ public struct COCOCategory: Decodable {
     var supercategory: String
     var id: Int
     var name: String
-    var keypoints: [String]
-    var skeleton: [COCOSize]
+    var keypoints: [String]?
+    var skeleton: [[Int]]?
     public init(
-        supercategory: String, id: Int, name: String, keypoints: [String], skeleton: [COCOSize]
+        supercategory: String, id: Int, name: String, keypoints: [String], skeleton: [[Int]]
     ) {
         self.supercategory = supercategory
         self.id = id
@@ -130,15 +110,19 @@ public struct COCOCategory: Decodable {
     }
 }
 public struct COCOMetadata: Decodable {
-    var info: COCOInfo
-    var licenses: [COCOLicense]
+    var info: COCOInfo?
+    var licenses: [COCOLicense]?
     var images: [COCOImage]
-    var annotations: [COCOAnnotation]
-    var type: String
-    var categories: [COCOCategory]
+    var annotations: [COCOAnnotation]?
+    var type: String? 
+    var categories: [COCOCategory]?
     public init(
-        info: COCOInfo, licenses: [COCOLicense], images: [COCOImage], annotations: [COCOAnnotation],
-        type: String, categories: [COCOCategory]
+        info: COCOInfo?,
+        licenses: [COCOLicense], 
+        images: [COCOImage], 
+        annotations: [COCOAnnotation],
+        type: String?, 
+        categories: [COCOCategory]
     ) {
         self.info = info
         self.licenses = licenses
--- Examples/Coco/main.swift
+++ Examples/Coco/main.swift
@@ -1,5 +1,34 @@
-import Datasets
 import Foundation
+import Datasets
+import SwiftProtobuf
 
-let fileURL = URL(fileURLWithPath: "pycocotools/annotations/pascal_val2012.json")
-let metadata = COCO(fromFile: fileURL)
+let fileNames = [
+    //"captions_train2017.json",
+    //"captions_val2017.json",
+    //"ILSVRC2014_train.json",
+    //"ILSVRC2014_val.json",
+    //"image_info_test2017.json",
+    //"image_info_test-dev2017.json",
+    //"image_info_unlabeled2017.json",
+    //"instances_train2017.json",
+    //"instances_val2017.json",
+    //"pascal_test2007.json",
+    //"pascal_train2007.json",
+    //"pascal_train2012.json",
+    //"pascal_val2007.json",
+    //"pascal_val2012.json",
+    //"person_keypoints_train2017.json",
+    //"person_keypoints_val2017.json",
+    "stuff_train2017.json",
+    //"stuff_val2017.json",
+]
+
+for fileName in fileNames {
+    let fileURL = URL(fileURLWithPath: "pycocotools/annotations/\(fileName)")
+    // Variant 1: codable json
+    let metadata = COCO(fromFile: fileURL)
+    // Variant 2: untyped json
+    // let contents = try String(contentsOfFile: fileURL.path)
+    // let data = contents.data(using: .utf8)!
+    // let metadata = try! JSONSerialization.jsonObject(with: data)
+}
