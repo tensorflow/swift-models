d5208c57f8e46f825d51a61b40739fccaa04f183
Denys Shabalin <shabalin@google.com>
2020-04-08 14:16:31 +0200

    wip

--- Datasets/Coco/COCO.swift
+++ Datasets/Coco/COCO.swift
@@ -3,22 +3,36 @@ import Foundation
 /// Coco dataset API that loads annotation file and prepares 
 /// data structures for data set access.
 public struct COCO {
-    let metadata: Any
+    let metadata: [String: Any]
     let anns: [String: Any]
     let cats: [String: Any]
     let imgs: [String: Any]
-    let imgToAnns: [String: Any]
-    let catToImgs: [String: Any]
+    let imgToAnns: [String: [Any]]
+    let catToImgs: [String: [Any]]
 
     public init(fromFile fileURL: URL) throws {
         let contents = try String(contentsOfFile: fileURL.path)
         let data = contents.data(using: .utf8)!
-        self.metadata = try JSONSerialization.jsonObject(with: data)
+        let parsed = try JSONSerialization.jsonObject(with: data)
+        self.metadata = parsed as [String: Any] 
         self.anns = [:]
         self.cats = [:]
         self.imgs = [:]
         self.imgToAnns = [:]
         self.catToImgs = [:]
+        self.createIndex()
+    }
+
+    func createIndex() {
+        if let annotations = metadata["annotations"] {
+            let anns = annotations as [[String: Any]]
+        }
+        if let images = metadata["images"] {
+        }
+        if let categories = metadata["categories"] {
+        }
+        if let annotations = metadata["annotations"] {
+        }
     }
 
     /// Get annotation ids that satisfy given filter conditions.
