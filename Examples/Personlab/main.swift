f99d86322170f4658be69a0036ad6dbb8fd98e4c
joaqo <joaquin.alori@gmail.com>
2020-05-03 19:21:53 -0300

    Add SwiftCV image loading from disc and from webcam

--- Examples/Personlab/main.swift
+++ Examples/Personlab/main.swift
@@ -1,30 +1,72 @@
 import Foundation
 import TensorFlow
 import ModelSupport
-import ImageClassificationModels
+import SwiftCV
 
 // Setup
 let config = Config()
 Context.local.learningPhase = .inference
-let checkpointFile = URL(fileURLWithPath: "/Users/joaqo/code/personlab/checkpoints/Personlab")
+let checkpointFile = URL(fileURLWithPath: config.checkPointPath)
 var ckpt = try! CheckpointReader(checkpointLocation: checkpointFile, modelName: "Personlab")
 
-// Pre-processing
-var image = Image(jpeg: URL(fileURLWithPath: config.testImagePath))
+// // Pre-processing
+// var image = Image(jpeg: URL(fileURLWithPath: config.testImagePath))
+// image = image.resized(to: config.inputImageSize)  // Adds a batch dimension automagically
+// var normalizedImagesTensor = image.tensor * (2.0 / 255.0) - 1.0
+
+var image = Image(tensor: Tensor<UInt8>(cvMat: imread(config.testImagePath))!)
 image = image.resized(to: config.inputImageSize)  // Adds a batch dimension automagically
 var normalizedImagesTensor = image.tensor * (2.0 / 255.0) - 1.0
+normalizedImagesTensor = _Raw.reverse(normalizedImagesTensor, dims: [false, false, false, true])
 
 // Define network and load pre trained weights
 let backbone = MobileNetLikeBackbone(checkpoint: ckpt)
 let personlabHeads = PersonlabHeads(checkpoint: ckpt)
 
 // Run pose estimator
+let startTime = Date()
 var convnetResults = personlabHeads(backbone(normalizedImagesTensor))
+let convnetTime = Date()
 let poseDecoder = PoseDecoder(for: convnetResults, with: config)
 var poses = poseDecoder.decode()
 
-// Results
 for pose in poses {
   print(pose)
 }
+
 print("Number of poses", poses.count)
+print("Convnet seconds", convnetTime.timeIntervalSince(startTime))
+print("Decoder seconds", Date().timeIntervalSince(convnetTime))
+
+// let cap = VideoCapture(0)
+// // Optional, reduces latency a bit
+// cap.set(VideoCaptureProperties.CAP_PROP_BUFFERSIZE, 1)
+// 
+// let frame = Mat()
+// let point1 = Point(x: 1, y: 10)
+// let point2 = Point(x: 302, y: 402)
+// let point3 = Point(x: 200, y: 402)
+// let color = Scalar(val1: 0, val2: 255, val3: 255, val4: 1)
+// 
+// while true {
+//   // Get tensor
+//   cap.read(into: frame)
+//   var image = Image(tensor: Tensor<UInt8>(cvMat: frame)!)
+//   image = image.resized(to: config.inputImageSize)  // Adds a batch dimension automagically
+//   let normalizedImagesTensor = image.tensor * (2.0 / 255.0) - 1.0
+// 
+//   // Run pose estimator
+//   let startTime = Date()
+//   let convnetResults = personlabHeads(backbone(normalizedImagesTensor))
+//   let convnetTime = Date()
+//   let poseDecoder = PoseDecoder(for: convnetResults, with: config)
+//   let poses = poseDecoder.decode()
+//   line(img: frame, pt1: point1, pt2: point2, color: color, thickness: 3)
+//   ImShow(image: frame)
+//   WaitKey(delay: 1)
+// 
+//   // Results
+//   print("Number of poses", poses.count)
+//   print("Convnet seconds", convnetTime.timeIntervalSince(startTime))
+//   print("Decoder seconds", Date().timeIntervalSince(convnetTime))
+// }
--- Examples/Personlab/utils.swift
+++ Examples/Personlab/utils.swift
@@ -3,10 +3,12 @@ import ModelSupport
 
 
 struct Config {
-    // Image
+    // Input image
     let inputImageSize = (height:241, width: 289)
-    let testImagePath = "/Users/joaqo/swift-models/pose.jpg"
-    // let testImagePath = "/Users/joaqo/Desktop/pose2.jpg"
+    let testImagePath = "/home/lalo/swift-models/Examples/Personlab/pose.jpg"
+
+    // Checkpoint
+    let checkPointPath = "/home/lalo/swift-models/Examples/Personlab/checkpoints/Personlab"
 
     // Decoder
     let outputStride = 16
@@ -14,7 +16,6 @@ struct Config {
     let keypointScoreThreshold: Float = 0.1
     let nmsRadius: Float = 20.0
     let keypointLocalMaximumRadius = 1  // TODO: what? they use 1 in implementation but not in paper
-    
 }
 
 
@@ -25,7 +26,7 @@ extension CheckpointReader {
 }
 
 
-/// Used as an ad hoc "hash" for tensor checking when copying the backbone from
+/// Used as an ad-hoc "hash" for tensor checking when copying the backbone from
 /// our Python Tensorflow 1.5 version
 func hash(_ tensor: Tensor<Float>) {
     print("[\(tensor.flattened().sum()), \(tensor[0, 0, 0]) \(tensor[0, -1, 1]), \(tensor[0, 1, 0]), \(tensor[0, -1, -1])]")
