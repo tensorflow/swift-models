0b2891f530f84f56c0360b3e344a179232456637
Snehal Ranjan <snehalranjan5472@gmail.com>
2020-05-11 21:46:24 +0530

    add tests

--- Tests/DatasetsTests/XCTestManifests.swift
+++ Tests/DatasetsTests/XCTestManifests.swift
@@ -24,6 +24,7 @@ import XCTest
             testCase(ImagenetteTests.allTests),
             testCase(BostonHousingTests.allTests),
             testCase(TextUnsupervisedTests.allTests),
+            testCase(OxfordIIITPetsTests.allTests),
         ]
     }
 #endif
