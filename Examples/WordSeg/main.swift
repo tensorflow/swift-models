8ec85117b99ad5f2d98b99444eb0eb19f9b7637d
Michelle Casbon <texasmichelle@users.noreply.github.com>
2020-05-18 13:03:37 -0700

    [WordSeg] Train until loss stops improving. (#542)
    * Train until loss stops improving.
    
    * Add loss check for training without validation

--- Examples/WordSeg/main.swift
+++ Examples/WordSeg/main.swift
@@ -22,7 +22,9 @@ let ndim = 512  // Hidden unit size.
 // Training flags
 let dropoutProb = 0.5  // Dropout rate.
 let order = 5  // Power of length penalty.
-let maxEpochs = 10  // Maximum number of training epochs.
+let maxEpochs = 1000  // Maximum number of training epochs.
+var lossHistory = Array<Float>() // Keep track of loss.
+var noImprovements = 0 // Consecutive epochs without improvements to loss.
 let lambd: Float = 0.00075  // Weight of length penalty.
 // Lexicon flags.
 let maxLength = 10  // Maximum length of a string.
@@ -93,12 +95,24 @@ for epoch in 1...maxEpochs {
   }
 
   guard let validationDataset = dataset.validation else {
+    let trainingLoss = trainingLossSum / Float(trainingBatchCount)
+
     print(
       """
       [Epoch \(epoch)] \
-      Training loss: \(trainingLossSum / Float(trainingBatchCount))
+      Training loss: \(trainingLoss))
       """
     )
+
+    // Stop training when loss stops improving.
+    lossHistory.append(trainingLoss)
+    if lossHistory.min() == trainingLoss {
+      noImprovements = 0
+    } else {
+      noImprovements += 1
+      if noImprovements >= 5 { break }
+    }
+
     continue
   }
 
@@ -116,14 +130,24 @@ for epoch in 1...maxEpochs {
   }
 
   let bpc = validationLossSum / Float(validationCharacterCount) / log(2)
+  let validationLoss = validationLossSum / Float(validationBatchCount)
 
   print(
     """
     [Epoch \(epoch)] \
     Bits per character: \(bpc) \
-    Validation loss: \(validationLossSum / Float(validationBatchCount))
+    Validation loss: \(validationLoss)
     """
   )
+
+  // Stop training when loss stops improving.
+  lossHistory.append(validationLoss)
+  if lossHistory.min() == validationLoss {
+    noImprovements = 0
+  } else {
+    noImprovements += 1
+    if noImprovements >= 5 { break }
+  }
 }
 
 func hasNaN<T: KeyPathIterable>(_ t: T) -> Bool {
