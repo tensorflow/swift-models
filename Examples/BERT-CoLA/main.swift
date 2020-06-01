74f18d8bdf4a28454d8edcc6025cb7157ec3ede4
Shadaj Laddad <shadaj@users.noreply.github.com>
2020-06-01 12:17:55 -0700

    Replace WeightDecayedAdam with an x10 optimizer for the BERT-CoLA example

--- Examples/BERT-CoLA/main.swift
+++ Examples/BERT-CoLA/main.swift
@@ -17,6 +17,10 @@ import Foundation
 import ModelSupport
 import TensorFlow
 import TextModels
+import x10_optimizers_optimizer
+
+// TODO(shadaj): Device.defaultXLA causes CUDA OOM
+let device = Device(kind: .GPU, ordinal: 0, backend: .XLA)
 
 let bertPretrained = BERT.PreTrainedModel.bertBase(cased: false, multilingual: false)
 let workspaceURL = URL(
@@ -26,6 +30,7 @@ let workspaceURL = URL(
         isDirectory: true))
 let bert = try BERT.PreTrainedModel.load(bertPretrained)(from: workspaceURL)
 var bertClassifier = BERTClassifier(bert: bert, classCount: 1)
+bertClassifier.move(to: device)
 
 // Regarding the batch size, note that the way batching is performed currently is that we bucket
 // input sequences based on their length (e.g., first bucket contains sequences of length 1 to 10,
@@ -55,17 +60,65 @@ var cola = try CoLA(
 
 print("Dataset acquired.")
 
-var optimizer = WeightDecayedAdam(
+public func makeWeightDecayedAdam(
+  learningRate: Float = 0.01,
+  beta1: Float = 0.9,
+  beta2: Float = 0.999,
+  weightDecayRate: Float = 0.01,
+  epsilon: Float = 1e-6
+) -> ParameterGroupOptimizer {
+  var b = ParameterGroupOptimizerBuilder()
+  let lr = b.makeParameter("learningRate", learningRate)
+  let beta1 = b.makeParameter("beta1", beta1)
+  let beta2 = b.makeParameter("beta2", beta2)
+  let wd = b.makeParameter("weightDecay", weightDecayRate)
+
+  let firstMoment = b[state: "firstMoment"]
+  let secondMoment = b[state: "secondMoment"]
+
+  b.appendCallback { (state: inout OptimizerWeightStepState, optState: inout OptimizerState) in
+    optState[state, firstMoment] =
+      state[beta1] * optState[state, firstMoment] + state.grad * (1 - state[beta1])
+  }
+
+  b.appendCallback { (state: inout OptimizerWeightStepState, optState: inout OptimizerState) in
+    optState[state, secondMoment] =
+      state[beta2] * optState[state, secondMoment] + state.grad .* state.grad * (1 - state[beta2])
+  }
+
+  b.appendCallback { (state: inout OptimizerWeightStepState, optState: inout OptimizerState) in
+    let denominator = sqrt(optState[state, secondMoment]).adding(epsilon)
+    let update = optState[state, firstMoment] ./ denominator + state.weight * state[wd]
+    state.step = -state[lr] * update
+  }
+
+  return b.makeOptimizer()
+}
+
+let beta1: Float = 0.9
+let beta2: Float = 0.999
+let useBiasCorrection = true
+
+var optimizer = x10_optimizers_optimizer.GeneralOptimizer(
     for: bertClassifier,
-    scheduledLearningRate: LinearlyDecayedParameter(
-        baseParameter: LinearlyWarmedUpParameter(
-            baseParameter: FixedParameter<Float>(2e-5),
-            warmUpStepCount: 10,
-            warmUpOffset: 0),
-        slope: -5e-7,  // The LR decays linearly to zero in 100 steps.
-        startStep: 10),
-    weightDecayRate: 0.01,
-    maxGradientGlobalNorm: 1)
+    TensorVisitorPlan(bertClassifier.differentiableVectorView),
+    defaultOptimizer: makeWeightDecayedAdam(
+      learningRate: 2e-5,
+      beta1: beta1,
+      beta2: beta2
+    )
+)
+
+// TOOD(shadaj): 1068 is a magic number for the number of batches in each epoch, where does this come from?
+// TODO(shadaj): learning rate originally went to zero very early, changed to decay until the last step
+var scheduledLearningRate = LinearlyDecayedParameter(
+  baseParameter: LinearlyWarmedUpParameter(
+      baseParameter: FixedParameter<Float>(2e-5),
+      warmUpStepCount: 10,
+      warmUpOffset: 0),
+  slope: -(2e-5 / (1068 * 3)),  // The LR decays linearly to zero in 3*1068 steps.
+  startStep: 10
+)
 
 print("Training BERT for the CoLA task!")
 for (epoch, epochBatches) in cola.trainingEpochs.prefix(3).enumerated() {
@@ -75,8 +128,14 @@ for (epoch, epochBatches) in cola.trainingEpochs.prefix(3).enumerated() {
     var trainingBatchCount = 0
 
     for batch in epochBatches {
-        let (documents, labels) = (batch.data, Tensor<Float>(batch.label))
-        let (loss, gradients) = valueWithGradient(at: bertClassifier) { model -> Tensor<Float> in
+        let (eagerDocuments, eagerLabels) = (batch.data, Tensor<Float>(batch.label))
+        let documents = TextBatch(
+          tokenIds: Tensor(copying: eagerDocuments.tokenIds, to: device),
+          tokenTypeIds: Tensor(copying: eagerDocuments.tokenTypeIds, to: device),
+          mask: Tensor(copying: eagerDocuments.mask, to: device)
+        )
+        let labels = Tensor(copying: eagerLabels, to: device)
+        var (loss, gradients) = valueWithGradient(at: bertClassifier) { model -> Tensor<Float> in
             let logits = model(documents)
             return sigmoidCrossEntropy(
                 logits: logits.squeezingShape(at: -1),
@@ -86,7 +145,17 @@ for (epoch, epochBatches) in cola.trainingEpochs.prefix(3).enumerated() {
 
         trainingLossSum += loss.scalarized()
         trainingBatchCount += 1
+        gradients.clipByGlobalNorm(clipNorm: 1)
+
+        let step = optimizer.step + 1 // for scheduled rates and bias correction, steps start at 1
+        optimizer.learningRate = scheduledLearningRate(forStep: UInt64(step))
+        if useBiasCorrection {
+          let step = Float(step)
+          optimizer.learningRate *= sqrtf(1 - powf(beta2, step)) / (1 - powf(beta1, step))
+        }
+
         optimizer.update(&bertClassifier, along: gradients)
+        LazyTensorBarrier()
 
         print(
             """
@@ -101,11 +170,17 @@ for (epoch, epochBatches) in cola.trainingEpochs.prefix(3).enumerated() {
     var devPredictedLabels = [Bool]()
     var devGroundTruth = [Bool]()
     for batch in cola.validationBatches {
-        let (documents, labels) = (batch.data, Tensor<Float>(batch.label))
+        let (eagerDocuments, eagerLabels) = (batch.data, Tensor<Float>(batch.label))
+        let documents = TextBatch(
+          tokenIds: Tensor(copying: eagerDocuments.tokenIds, to: device),
+          tokenTypeIds: Tensor(copying: eagerDocuments.tokenTypeIds, to: device),
+          mask: Tensor(copying: eagerDocuments.mask, to: device)
+        )
+        let labels = Tensor(copying: eagerLabels, to: device)
         let logits = bertClassifier(documents)
         let loss = sigmoidCrossEntropy(
             logits: logits.squeezingShape(at: -1),
-            labels: Tensor<Float>(labels),
+            labels: labels,
             reduction: { $0.mean() }
         )
         devLossSum += loss.scalarized()
