db50d5e567002698a55edc3406f16eaab2aa73f1
joaqo <joaquin.alori@gmail.com>
2020-06-12 17:01:52 -0300

    Add copyright headers to PersonLab files

--- PersonLab/Swiftcv.swift
+++ PersonLab/Swiftcv.swift
@@ -1,3 +1,17 @@
+// Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import SwiftCV
 // Taken from: https://github.com/fastai/course-v3/blob/21402bf741fe4325dd217e6739d95e4b28101ee3/nbs/swift/SwiftCV/Sources/SwiftCV/TensorFlowConversion.swift
 import TensorFlow
