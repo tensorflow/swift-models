0a5feec4257303d29a2ad5513ca2c8a0b1771789
Shadaj Laddad <shadaj@users.noreply.github.com>
2020-07-22 10:29:36 -0700

    Rename on parameters to on device

--- Datasets/CoLA/CoLA.swift
+++ Datasets/CoLA/CoLA.swift
@@ -114,7 +114,7 @@ extension CoLA {
     maxSequenceLength: Int,
     batchSize: Int,
     entropy: Entropy,
-    on: Device,
+    on device: Device,
     exampleMap: @escaping (CoLAExample) -> LabeledTextBatch
   ) throws {
     self.directoryURL = taskDirectoryURL.appendingPathComponent("CoLA")
@@ -159,8 +159,8 @@ extension CoLA {
     ).lazy.map { (batches: Batches) -> LazyMapSequence<Batches, LabeledTextBatch> in
       batches.lazy.map{ 
         (
-          data: $0.map(\.data).paddedAndCollated(on: on, to: maxSequenceLength),
-          label: Tensor(copying: Tensor($0.map(\.label)), to: on)
+          data: $0.map(\.data).paddedAndCollated(on: device, to: maxSequenceLength),
+          label: Tensor(copying: Tensor($0.map(\.label)), to: device)
         )
       }
     }
@@ -168,8 +168,8 @@ extension CoLA {
     // Create the validation collection of batches.
     validationBatches = validationExamples.inBatches(of: batchSize / maxSequenceLength).lazy.map{ 
       (
-        data: $0.map(\.data).paddedAndCollated(on: on, to: maxSequenceLength),
-        label: Tensor(copying: Tensor($0.map(\.label)), to: on)
+        data: $0.map(\.data).paddedAndCollated(on: device, to: maxSequenceLength),
+        label: Tensor(copying: Tensor($0.map(\.label)), to: device)
       )
     }
   }
--- Support/Text/TextBatch.swift
+++ Support/Text/TextBatch.swift
@@ -55,20 +55,20 @@ extension TextBatch: Collatable {
 extension Collection where Element == TextBatch {
   /// Returns the elements of `self`, padded to `maxLength` if specified
   /// or the maximum length of the elements in `self` otherwise.
-  public func paddedAndCollated(on: Device, to maxLength: Int? = nil) -> TextBatch {
+  public func paddedAndCollated(on device: Device, to maxLength: Int? = nil) -> TextBatch {
     let maxLength = maxLength ?? self.map { $0.tokenIds.shape[1] }.max()!
     let paddedTexts = self.map { text -> TextBatch in
       let paddingSize = maxLength - text.tokenIds.shape[1]
       return TextBatch(
         tokenIds: Tensor(copying: text.tokenIds.padded(forSizes: [
           (before: 0, after: 0),
-          (before: 0, after: paddingSize)]), to: on),
+          (before: 0, after: paddingSize)]), to: device),
         tokenTypeIds: Tensor(copying: text.tokenTypeIds.padded(forSizes: [
           (before: 0, after: 0),
-          (before: 0, after: paddingSize)]), to: on),
+          (before: 0, after: paddingSize)]), to: device),
         mask: Tensor(copying: text.mask.padded(forSizes: [
           (before: 0, after: 0),
-          (before: 0, after: paddingSize)]), to: on))
+          (before: 0, after: paddingSize)]), to: device))
     }
 
     if count == 1 { return paddedTexts.first! }
