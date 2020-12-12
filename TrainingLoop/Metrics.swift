54f967e061dd181503aac1d64281321c7517a224
brett koonce <koonce@gmail.com>
2020-12-12 03:03:45 +0000

    use topk operator

--- TrainingLoop/Metrics.swift
+++ TrainingLoop/Metrics.swift
@@ -30,7 +30,9 @@ public enum TrainingMetrics {
     case .accuracy:
       return AccuracyMeasurer(self.name)
     case .top5:
-      return AccuracyMeasurerTop5(self.name)
+      var measurer = AccuracyMeasurer(self.name)
+      measurer.k = 5
+      return measurer
     case .matthewsCorrelationCoefficient:
       return MCCMeasurer(self.name)
     case .perplexity:
@@ -94,10 +96,11 @@ public struct LossMeasurer: MetricsMeasurer {
   }
 }
 
-/// A measurer for measuring accuracy (top 1)
+/// A measurer for measuring accuracy (top k)
 public struct AccuracyMeasurer: MetricsMeasurer {
   /// Name of the AccuracyMeasurer.
   public var name: String
+  public var k: Int32 = 1
 
   /// Count of correct guesses.
   private var correctGuessCount: Int32 = 0
@@ -105,7 +108,7 @@ public struct AccuracyMeasurer: MetricsMeasurer {
   /// Count of total guesses.
   private var totalGuessCount: Int32 = 0
 
-  /// Creates an instance with the AccuracyMeasurer named `name`. 
+  /// Creates an instance with the AccuracyMeasurerTop5 named `name`. 
   public init(_ name: String = "accuracy") {
     self.name = name
   }
@@ -128,58 +131,9 @@ public struct AccuracyMeasurer: MetricsMeasurer {
         "For accuracy measurements, the model output must be Tensor<Float>, and the labels must be Tensor<Int>."
       )
     }
-    correctGuessCount += Tensor<Int32>(predictions.argmax(squeezingAxis: -1) .== labels).sum()
-      .scalarized()
-    totalGuessCount += Int32(labels.shape.reduce(1, *))
-  }
-
-  /// Computes accuracy as percentage of correct guesses.
-  public func measure() -> Float {
-    return Float(correctGuessCount) / Float(totalGuessCount)
-  }
-}
-
-/// A measurer for measuring accuracy (top 5)
-public struct AccuracyMeasurerTop5: MetricsMeasurer {
-  /// Name of the AccuracyMeasurer.
-  public var name: String
-
-  /// Count of correct guesses.
-  private var correctGuessCount: Int32 = 0
-
-  /// Count of total guesses.
-  private var totalGuessCount: Int32 = 0
-
-  /// Creates an instance with the AccuracyMeasurerTop5 named `name`. 
-  public init(_ name: String = "top5") {
-    self.name = name
-  }
-
-  /// Resets correctGuessCount and totalGuessCount to zero.
-  public mutating func reset() {
-    correctGuessCount = 0
-    totalGuessCount = 0
-  }
-
-  /// Computes correct guess count from `loss`, `predictions` and `labels`
-  /// and adds it to correctGuessCount; Computes total guess count from
-  /// `labels` shape and adds it to totalGuessCount.
-  public mutating func accumulate<Output, Target>(
-    loss: Tensor<Float>?, predictions: Output?, labels: Target?
-  ) {
-    guard let predictions = predictions as? Tensor<Float>, let labels = labels as? Tensor<Int32>
-    else {
-      fatalError(
-        "For accuracy measurements, the model output must be Tensor<Float>, and the labels must be Tensor<Int>."
-      )
-    }
-    let top5 = _Raw.topKV2(predictions, k: Tensor<Int32>(5), sorted: false)
-    let batchSize = labels.shape[0]
-    let labelsReshaped = labels.reshaped(to: [1, batchSize])
-    let labelsTranspose = labelsReshaped.transposed(permutation: [1, 0])
-    let ones = Tensor<Int32>([1, 1, 1, 1, 1])
-    let expandedLabels = labelsTranspose * ones
-    correctGuessCount += Tensor<Int32>(top5.indices .== expandedLabels).sum().scalarized()
+    let topK = _Raw.inTopKV2(predictions: predictions, targets: labels, k: Tensor<Int32>(k))
+    let correctCount = Tensor<Int32>(topK).sum()
+    correctGuessCount += Int32(correctCount.scalar ?? 0)
     totalGuessCount += Int32(labels.shape.reduce(1, *))
   }
 
